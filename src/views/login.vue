fcb88e69ab2ab862cb573f53f78d151d1a9699fa
h0341a <qianaiwuyu@gmail.com>
2019-12-23 16:13:33 +0800

    install less-loader add login view route the login page to the default interface add style folder,two less files were pulled from the reference project

---
+++ src/views/login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login_page fillcontain">
+    <section class="form_contianer">
+      <div class="manage_tip">
+        <p>博客后台管理系统</p>
+      </div>
+      <el-form>
+        <el-form-item>
+          <el-input v-model="username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="password" placeholder="密码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="submit_btn">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "../style/mixin";
+.login_page {
+  background-color: #324057;
+}
+.manage_tip {
+  position: absolute;
+  width: 100%;
+  top: -100px;
+  left: 0;
+  p {
+    font-size: 34px;
+    color: #fff;
+  }
+}
+.form_contianer {
+  .wh(320px, 210px);
+  .ctp(320px, 210px);
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #fff;
+  .submit_btn {
+    width: 100%;
+    font-size: 16px;
+  }
+}
+</style>
