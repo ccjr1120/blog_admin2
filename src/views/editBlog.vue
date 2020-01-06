62dffbe64aa133a422934944b71a7fb0b8204193
qianaiwuyu@gmail.com <qianaiwuyu@gmail.com>
2020-01-06 20:05:50 +0800

    Optimized the experience of editor. Added two new functios.These is save And auto save.

--- src/views/editBlog.vue
+++ src/views/editBlog.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="mavonEditor">
-    <mavon-editor v-model="value"/>
+  <div>
+    <div class="mavonEditor">
+      <mavon-editor
+      @change="saveBlogCache"
+      @save="saveBlog"
+       v-model="blogContent" />
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      handbook: "#### how to use mavonEditor in nuxt.js"
+      blogContent: ""
     };
+  },
+  methods: {
+    saveBlogCache(){
+      window.console.log(this.blogContent)
+    },
+    saveBlog() {
+      alert(this.blogContent);
+    }
   }
 };
 </script>
@@ -17,5 +30,11 @@ export default {
 .mavonEditor {
   width: 100%;
   height: 100%;
+}
+.btn {
+  float: right;
+  padding-top: 6px;
+  padding-right: 0px;
+  padding-left: 16px;
 }
 </style>
