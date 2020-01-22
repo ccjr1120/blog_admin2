d1999be5b8ee13be53b284016e265e3d35d4b567
h0341a <qianaiwuyu@gmail.com>
2020-01-22 22:12:25 +0800

    finish get comment list from back end

--- src/views/comments.vue
+++ src/views/comments.vue
@@ -1,7 +1,8 @@
 <template>
   <el-table :data="tableData" border style="width: 100%">
-    <el-table-column prop="id" label="评论ID" width="180"></el-table-column>
-    <el-table-column prop="date" label="日期" width="180"></el-table-column>
+    <el-table-column prop="cid" label="评论ID" width="180"></el-table-column>
+    <el-table-column prop="parent" label="是否为父评论" width="180"></el-table-column>
+    <el-table-column prop="createDate" label="日期" width="180"></el-table-column>
     <el-table-column prop="blogTitle" label="博客标题"></el-table-column>
     <el-table-column prop="content" label="评论内容"></el-table-column>
     <el-table-column fixed="right" label="操作" width="200">
@@ -19,13 +20,22 @@ export default {
     return {
       tableData: [
         {
-          id: "1",
-          date: "2016-05-02",
-          content: "comment",
-          blogTitle: "why the sun alwas here?"
+          cid: "1",
+          parent: 'true',
+          createDate: "2016-05-02",
+          content: "comment example1",
+          blogTitle: "blog title example1"
         }
       ]
     };
+  },
+  created:function(){
+    this.$axios.get("/admin/comment/all").then(resp=>{
+      var data = resp.data;
+      if(data.success){
+        this.tableData = data.data;
+      }
+    })
   }
 };
 </script>
